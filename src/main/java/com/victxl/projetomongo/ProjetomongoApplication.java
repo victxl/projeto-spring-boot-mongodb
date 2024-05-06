3faef34ca4a12fcab2fd4ead880f71d0296941ea
Victor Henrique <65699690+victxl@users.noreply.github.com>
2024-05-05 22:45:03 -0300

    Primeiro Commit

---
+++ src/main/java/com/victxl/projetomongo/ProjetomongoApplication.java
@@ -0,0 +1,13 @@
+package com.victxl.projetomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victxl/projetomongo/ProjetomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victxl.projetomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
