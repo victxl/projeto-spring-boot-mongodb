3e55b0250d88b9bf4d714d3e62afffcc47ec64d1
Victor Henrique <65699690+victxl@users.noreply.github.com>
2024-05-06 00:58:47 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/victxl/projetomongo/resources/UsuarioResource.java
+++ src/main/java/com/victxl/projetomongo/resources/UsuarioResource.java
@@ -5,11 +5,10 @@ import com.victxl.projetomongo.dto.UsuarioDTO;
 import com.victxl.projetomongo.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,11 +25,27 @@ public class UsuarioResource {
         List<UsuarioDTO> listDTO = list.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<UsuarioDTO> findById(@PathVariable String id) {
-        Usuario obj = usuarioService.findById(id);
-        return ResponseEntity.ok().body(new UsuarioDTO(obj));
+        Usuario usuario = usuarioService.findById(id);
+        return ResponseEntity.ok().body(new UsuarioDTO(usuario));
     }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioService.fromDTO(usuarioDTO);
+        usuario = usuarioService.insert(usuario);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        usuarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
--- src/main/java/com/victxl/projetomongo/services/UsuarioService.java
+++ src/main/java/com/victxl/projetomongo/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.victxl.projetomongo.services;
 
 import com.victxl.projetomongo.domain.Usuario;
+import com.victxl.projetomongo.dto.UsuarioDTO;
 import com.victxl.projetomongo.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,8 +20,24 @@ public class UsuarioService {
 
 
     }
+
     public Usuario findById(String id) {
-       Optional<Usuario> usuario = usuarioRepository.findById(id);
-       return usuario.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public Usuario insert(Usuario usuario) {
+        return usuarioRepository.insert(usuario);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        usuarioRepository.deleteById(id);
+    }
+
+    public Usuario fromDTO(UsuarioDTO usuarioDTO) {
+        return new Usuario(usuarioDTO.getId(), usuarioDTO.getNome(), usuarioDTO.getEmail());
+    }
+
+
 }
